3d67bdf8f142b91b73386c0653b7c3c0d480efc4
TOPOF <TOPOF@DESKTOP-JBUMQPF>
2018-09-22 13:22:37 -0300

    Clase partida falta la calse ficha

--- src/Obligatorio/Partida.java
+++ src/Obligatorio/Partida.java
@@ -10,7 +10,41 @@ package Obligatorio;
  * @author TOPOF
  */
 public class Partida {
-    private Jugador unJugador;
+    
+    private Jugador jugadoruno;
+    private Jugador jugadordos;
+    //private Ficha unaFicha;
+
+    public Partida() {
+        this.setJugadoruno(new Jugador());
+        this.setJugadordos(new Jugador());
+        
+    }
+
+    public Partida (Jugador Jugador1, Jugador Jugador2) {
+        this.setJugadoruno(Jugador1);
+        this.setJugadordos(Jugador2);
+    }
+
+    public Jugador getJugadoruno() {
+        return jugadoruno;
+    }
+
+    public void setJugadoruno(Jugador Jugador1) {
+        this.jugadoruno = Jugador1;
+    }
+
+    public Jugador getJugadordos() {
+        return jugadordos;
+    }
+
+    public void setJugadordos(Jugador Jugador2) {
+        this.jugadordos = Jugador2;
+    }
+    
+    
+    
+    
     
     
 }
