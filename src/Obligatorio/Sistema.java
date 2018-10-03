9e7d06a460d4141f2cac1793dea45dc4f59052c7
Santiago Rugnitz <santiagorugnitz@gmail.com>
2018-10-03 16:23:12 -0300

    por las dudas

--- src/Obligatorio/Sistema.java
+++ src/Obligatorio/Sistema.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor
  */
 package Obligatorio;
 
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Santiago Rügnitz y Franco Galeano
+ * @author TOPOF
  */
 public class Sistema {
 
