275fa68c69586edf69323bd33c52316a110312b3
Santiago Rugnitz <santiagorugnitz@gmail.com>
2018-10-23 17:00:05 -0300

    Hice las clases nuevas y un main nuevo

---
+++ src/Obligatorio/Inicio.java
@@ -0,0 +1,16 @@
+
+package Obligatorio;
+
+/**
+ *
+ * @author Santiago
+ */
+public class Inicio {
+
+    public static void main(String[] args) {
+        Sistema s = new Sistema(); //hacerlo bien con el file
+        Menu v = new Menu(s);
+        
+    }
+    
+}
